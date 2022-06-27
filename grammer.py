40da22f07133fe5abfcfb143abf1fc3d4ed0cd65
Adnan <alamin493641@gmail.com>
2022-06-27 14:57:37 +0600

    many article are add

--- grammer.py
+++ grammer.py
@@ -3,11 +3,21 @@ from happytransformer import TTSettings
 
 happy_tt = HappyTextToText("T5", "prithivida/grammar_error_correcter_v1")
 
-text = "gec: " + " Whot do someone thinks of grammar checkers? Please not that they are not perfect. Style problems get a blue marker: It is 7 P.M. in the evening. The weather was nice on Monday, 22 November 2021"
-settings = TTSettings(do_sample=True, top_k=10, temperature=0.5,  min_length=1, max_length=100)
-result = happy_tt.generate_text(text, args=settings)
+input='Twitter is a web services for friends, family, and colleagues to staying in touch with and stay connection through out fast, frequent messaging. People post in which includes photos, videos, links also texts by twitter accounts. Twitter is an social site through which it is easying to communication with considerable people. It is a worldwide social networking service and mac logging. Posting something on other social media is called status, just like this posting something on twitter is called tweet. This is a special social service app. It is a networking service in America. First of all, you should know its proper use'
+inlen=len(input)
 
-print(result.text)
+out = []
+threshold = 50
+for chunk in input.split('. '):
+    print('chunk',chunk)
+    text = "gec: " + chunk
+    settings = TTSettings(do_sample=True, top_k=10, temperature=0.5,  min_length=1, max_length=100)
+    result = happy_tt.generate_text(text, args=settings)
+
+# print(result.text)
+
+print('len',inlen)
+
 
 # import language_tool_python 
 # my_tool = language_tool_python.LanguageTool('en-US')
